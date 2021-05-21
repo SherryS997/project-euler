0d8b4c130cc24ad10853c8ebb6efbeaf2a9802fa
SherryS997 <kratosreborn32@protonmail.com>
2021-05-21 14:07:55 +0530

    dd

--- interview_q.py
+++ interview_q.py
@@ -11,18 +11,14 @@ while True:
             no = int(x/2)
             temp_nums.append(no)
             temp_nums.append(x - no)
-        else:
-            temp_nums.append(x)
+        else: temp_nums.append(x)
     nums = temp_nums
     for y in nums:
-        if y not in test:
+        if y not in test: 
             test.append(y)
-            if y > test_val:
-                test_val = y
-    if len(test) <= 2 and test_val <= 3:
-        break
+            if y > test_val: test_val = y
+    if len(test) <= 2 and test_val <= 3: break
 
-for z in nums:
-    ans *= z
-
-print(ans)
+for z in nums: ans *= z
+    
+print('Largest sum product =', ans)
